5de8afd5b9d25c6cf15c120ce4e16d47f49e27c2
Zach Hartwig <hartwig@psfc.mit.edu>
2013-05-23 18:15:34 +0000

    Adding the ACRONYM acroEvent and AAParallelResults classes to repo. Forget them before.

---
+++ include/AAParallelResults.hh
@@ -0,0 +1,15 @@
+#ifndef __AAParallelResults_hh__
+#define __AAParallelResults_hh__  1
+
+#include <TObject.h>
+
+class AAParallelResults : public TObject
+{
+public:
+  double TotalDeuterons;
+  
+  ClassDef(AAParallelResults, 1);
+};
+
+
+#endif
---
+++ include/acroEvent.hh
@@ -0,0 +1,27 @@
+#ifndef acroEvent_hh
+#define acroEvent_hh 1
+
+#include "TObject.h"
+#include "TString.h"
+
+class acroEvent : public TObject {
+public:
+  acroEvent(){;}
+  ~acroEvent(){;}
+
+  void Reset(){;}
+
+  // The recoil particle
+
+  Double_t recoilEnergyDep;
+  Double_t recoilLightOutput;
+
+  // The scintillation photons
+
+  Int_t scintPhotonsCreated; // in the scintillator
+  Int_t scintPhotonsCounted; // by the readout device
+
+  ClassDef(acroEvent,1)
+};
+
+#endif
