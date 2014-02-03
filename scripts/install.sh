61c6ab05c69d5937fffc46b12565103533bc44c4
Zach Hartwig <hartwig@psfc.mit.edu>
2014-02-03 13:59:42 -0500

    Removed binary from repo; updates to install script
    Former-commit-id: d722388bf1e47e8859b63c9546e6ac29887a3cc0

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # name: install.sh
-# date: 02 Feb 14
+# date: 03 Feb 14
 # auth: Zach Hartwig
 #
 # desc: Bash script for installing production version ADAQAnalysis
@@ -9,28 +9,53 @@
 #       accessible directory /usr/local/adaq. The list of production
 #       versions may be obtained by running "git tags"
 #
-# 2run: (sudo -E) ./install <Version.number.ID> 
+# 2run: ./install <Git.Version.Tag> 
 
 # Check to ensure the user has specified the version to copy
 
 if [ ! "$#" == "1" ]; then
     echo -e "\nInstall script error! Only a single argument is allowed from the user:"
     echo -e   "   arg1 == ADAQAnalysis version number"
-    echo -e   "   example: ./install 1.3.2"
+    echo -e   "   example: ./install.sh v1.3.2"
     exit
 fi
 
 # Set the version number
 VERSION=$1
 
+# Check to ensure that the desired version exists
+AVAILABLE=$(git tag)
+if [[ $AVAILABLE != *$VERSION* ]]; then
+    echo -e "\nInstall script error! The available versions are:"
+    echo -e   "--> $AVAILABLE\n"
+    exit
+fi
 
 
+# Set the installation location
+INSTALLDIR=/usr/local/adaq
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOPDIR=${SCRIPTDIR///scripts/}
 
+# Checkout the tagged version to a separate branch, build and install
+# the binaries, and the remove the temporary branch
+cd $TOPDIR
 
+echo -e "\nChecking out tagged version from the Git repository ..."
+git co $VERSION -b $VERSION >& /dev/null
 
+echo -e "\nBuilding the ADAQAnalysis binaries ..."
+make clean >& /dev/null
+make -j2 both >& /dev/null
 
-echo -e "***********************************************"
-echo -e "**   ADAQAnalysisGUI has been installed in:"
-echo -e "**        $INSTALL_DIR"
-echo -e "***********************************************\n"
+echo -e "\nCopying the binaries to $INSTALLDIR ..."
+sudo cp bin/* $INSTALLDIR >& /dev/null
+
+echo -e "\nRemoving the temporary tag branch ..."
+git br -d $VERSION >& /dev/null
+
+echo -e "\n*********************************************"
+echo -e   "**   ADAQAnalysis has been installed in:   **"
+echo -e   "**           $INSTALLDIR               **"
+echo -e   "*********************************************\n"
